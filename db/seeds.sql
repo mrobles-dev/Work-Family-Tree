67651eb3f10f8afba1534105478f3f492507f7f7
mrobles-dev <miltonjrobles1996@gmail.com>
2023-06-02 17:23:47 -0500

    working on switch case logic

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
 INSERT INTO department (department_name)
-VALUES (' fire')
+VALUES (' fire'),
 ('air')
 
 
 
 INSERT INTO roles (title, salary department_id)
-VALUES ( ' prince', '5000', 1)
-        ('avatar', '3000', 2)
+VALUES ( 'Fire Prince', '5000', 1),
+        ('Avatar', '3000', 2)
 
 
 
 
 INSERT INTO employees( first_name, last_name, role_id, manager_id)
-VALUES ( 'Zuko', 'smith', 1, NULL),
-       ( 'Aang', ' mendez', 2, NULL),
+VALUES ( 'Zuko', 'Rodriguez', 1, NULL),
+       ( 'Aang', ' Melendez', 2, NULL),
       
--- index.js
+++ index.js
@@ -18,17 +18,85 @@ inquirer
 .prompt([
   {
     type: "list",
-    message: "Would you like to add a dept?",
-    name: "text",
-    choices: ['water',
-    "earth",
-    " fire", 
-    "air"]
+    message: "What would you like to view/do?",
+    name: "choices",
+    choices: [
+    'View all depts',
+    "add dept",
+    "View all employees", 
+    "add employee",
+    'View all roles',
+    "update roll",
+    "Esc"
+  ]
   },
-]).then((res) => {
-  console.log(res);
+])
+// .then((res) => {
+//   console.log(res);
+//   //create switch case for choices above to be shown/added/updated
+// })
+.then((res) => {
+  
+  switch(res.choices){
+    case 'View all depts':
+      viewDepts();
+      break;
+    case 'add dept':
+      addDept();
+      break;
+    case 'View all employees':
+      viewEmployees();
+      break;
+    case 'add employee':
+      addEmployee();
+      break;
+    case 'View all roles':
+      viewRolls();
+      break;
+    case 'update role':
+      updateRole();
+      break;
+    case 'update role':
+      console.log('esc selected');
+      return;
+     
+  }
 });
+
+function viewDepts(){
+  db.query('SELECT * FROM department', function(results){
+    console.table(results);
+    userPrompt();
+  });
 }
+
+
+function addDept(){
+  inquirer.prompt([
+    {
+      type:'input',
+      message: "Name of new dept?",
+      name:'department'
+
+    }
+  ]).then ((res)=>
+  {
+    console.log(res);
+    db.query(`INSET INTO department SET name = ?`,[res.department], function (err){
+      if (err){
+        throw err;
+      }
+    })
+  })
+};
+
+
+
+}
+
+
+
+
 
 
 db.connect(err=>{
@@ -36,4 +104,4 @@ db.connect(err=>{
     userPrompt();
 }
     
-) 
+);
